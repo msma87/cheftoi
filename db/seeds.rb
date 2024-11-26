12f48c3cdece257100c8845ea27fac7c0f996025
Raquel Sylos <raquelsylos@gmail.com>
2024-11-26 15:16:57 -0300

    seeds services

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,31 @@
 # Limpando o banco de dados
+Service.destroy_all
 User.destroy_all
 
 # Criando usuários
 puts "Criando usuários..."
 
-# Somente atributos existentes no schema
 users = [
-  { email: "alice@gmail.com", password: "password" },
-  { email: "bruno@gmail.com", password: "password" },
-  { email: "carla@gmail.com", password: "password" },
-  { email: "daniel@gmail.com", password: "password" },
-  { email: "fernanda@gmail.com", password: "password" }
+  { email: "aliceveiga@gmail.com", password: "password" },
+  { email: "brunomartins@gmail.com", password: "password" },
+  { email: "carlaprieto@gmail.com", password: "password" }
 ]
 
-# Criando usuários no banco
-users.each do |user_data|
-  begin
-    User.create!(user_data)
-  rescue ActiveModel::UnknownAttributeError => e
-    puts "Erro ao criar usuário: #{e.message}"
-  end
-end
+user_records = users.map { |user_data| User.create!(user_data) }
 
 puts "#{User.count} usuários criados com sucesso!"
+
+# Criando serviços
+puts "Criando serviços..."
+
+services = [
+  { title: "Jantar Gourmet", category: "Gastronomia", price: 500.00, user: user_records[0] },
+  { title: "Buffet Vegano", category: "Vegano", price: 700.00, user: user_records[1] },
+  { title: "Churrasco Premium", category: "Churrasco", price: 800.00, user: user_records[2] },
+  { title: "Sobremesas Exclusivas", category: "Sobremesas", price: 300.00, user: user_records[0] },
+  { title: "Cozinha Internacional", category: "Internacional", price: 600.00, user: user_records[1] }
+]
+
+services.each { |service| Service.create!(service) }
+
+puts "#{Service.count} serviços criados com sucesso!"
