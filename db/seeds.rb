6851ec80cd5f73b7ff0108816a274201e7683d9e
Raquel Sylos <raquelsylos@gmail.com>
2024-11-26 11:58:44 -0300

    adding data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Limpando o banco de dados
+User.destroy_all
+
+# Criando usuários
+puts "Criando usuários..."
+
+# Somente atributos existentes no schema
+users = [
+  { email: "alice@gmail.com", password: "password" },
+  { email: "bruno@gmail.com", password: "password" },
+  { email: "carla@gmail.com", password: "password" },
+  { email: "daniel@gmail.com", password: "password" },
+  { email: "fernanda@gmail.com", password: "password" }
+]
+
+# Criando usuários no banco
+users.each do |user_data|
+  begin
+    User.create!(user_data)
+  rescue ActiveModel::UnknownAttributeError => e
+    puts "Erro ao criar usuário: #{e.message}"
+  end
+end
+
+puts "#{User.count} usuários criados com sucesso!"
