90f269220186b1820459cbd1664836c635625d41
Caio Castelhano <castelhanoc@gmail.com>
2024-11-26 16:30:53 -0300

    booking model created and validations and associations of booking, service e user updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  has_many :services, dependent: :destroy
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Relacionamentos
+  has_many :bookings, dependent: :destroy
+  has_many :services, through: :bookings
+
+  # Validações
+  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "não é válido" }
+  validates :email, uniqueness: { case_sensitive: false, message: "já está em uso" }
+  validates :password, length: { minimum: 6 }, if: -> { encrypted_password.blank? || !password.nil? }
+
+  # Devise
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_26_181040) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_26_185547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "service_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["service_id"], name: "index_bookings_on_service_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "services", force: :cascade do |t|
     t.string "title"
@@ -36,5 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_181040) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "services"
+  add_foreign_key "bookings", "users"
   add_foreign_key "services", "users"
 end
