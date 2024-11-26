fc995963913f43ded0656f3a58120a37871809a0
Maira Almeida <mairasenna@gmail.com>
2024-11-26 17:31:59 -0300

    Service delete

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,14 +1,22 @@
-# app/controllers/services_controller.rb - Máira
 class ServicesController < ApplicationController
-  before_action :set_service, only: [:show]
+  before_action :set_service, only: [:show, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
+  # ... outros métodos ...
   def index
-    @services = Service.all
+    @services = Service.all # Isso garante que @services será uma coleção, mesmo que vazia
   end
-
-  def show
-    @service = Service.find(params[:id])
+  
+  def destroy
+    if @service.user == current_user # Adiciona verificação de proprietário
+      if @service.destroy
+        redirect_to services_path, notice: 'Serviço deletado com sucesso.'
+      else
+        redirect_to service_path(@service), alert: 'Não foi possível deletar o serviço.'
+      end
+    else
+      redirect_to service_path(@service), alert: 'Você não tem permissão para deletar este serviço.'
+    end
   end
 
   private
@@ -18,6 +26,6 @@ class ServicesController < ApplicationController
   end
 
   def service_params
-    params.require(:service).permit(:title)
+    params.require(:service).permit(:title, :category, :price, :description) # Atualize com todos os campos permitidos
   end
 end
