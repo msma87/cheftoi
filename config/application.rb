5a1c1bed98f848bc0d1d078fd9cb74c2f18ef6ad
Maira Almeida <mairasenna@gmail.com>
2024-11-28 14:53:01 -0300

    correção arquivo application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,18 +6,18 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-mmodule ChefToi
-class Application < Rails::Application
-  config.action_controller.raise_on_missing_callback_actions = false if Rails.version >= "7.1.0"
-  config.generators do |generate|
-    generate.assets false
-    generate.helper false
-    generate.test_framework :test_unit, fixture: false
-  end
+module ChefToi
+  class Application < Rails::Application
+    config.action_controller.raise_on_missing_callback_actions = false if Rails.version >= "7.1.0"
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework :test_unit, fixture: false
+    end
 
-  config.load_defaults 7.1
-  config.i18n.default_locale = :'pt-BR'
-  config.time_zone = 'Brasilia'
-  config.autoload_lib(ignore: %w(assets tasks))
+    config.load_defaults 7.1
+    config.i18n.default_locale = :'pt-BR'
+    config.time_zone = 'Brasilia'
+    config.autoload_lib(ignore: %w(assets tasks))
+  end
 end
-end CopyRetryClaude does not have the ability to run the code it generates yet.
