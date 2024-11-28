6df768e4712a03e4ad9ff6ceff1c10143a3fa5f2
Caio Castelhano <castelhanoc@gmail.com>
2024-11-28 14:45:50 -0300

    sign up logic done

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create, :destroy]
   end
 
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
