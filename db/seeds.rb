4c86d21768f792c14c848e9f5c478b688eeff0e9
Maira Almeida <mairasenna@gmail.com>
2025-04-22 12:29:23 -0300

    imagens seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ services_data = [
     category: "Gastronomia Italiana",
     price: 350.00,
     description: "Prepare-se para uma experiência inesquecível com um menu completo que inclui entrada, prato principal e sobremesa, tudo inspirado na autêntica cozinha italiana. Ideal para ocasiões especiais.",
-    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1736888657/jantar-romantico_n5fftm.jpg",
+    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1745334696/jantar-romantico_n5fftm_esuqlc.jpg",
     user: users[0]
   },
   {
@@ -30,7 +30,7 @@ services_data = [
     category: "Gastronomia Vegana",
     price: 680.00,
     description: "Transforme seu evento com um buffet vegano que combina sabores inovadores e opções nutritivas. Inclui pratos quentes, saladas e sobremesas feitas com ingredientes frescos e locais.",
-    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1736887191/Vegan-Easter-Buffet_gvqqvl.jpg",
+    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1745334710/Vegan-Easter-Buffet_gvqqvl_bh99ct.jpg",
     user: users[1]
   },
   {
@@ -38,7 +38,7 @@ services_data = [
     category: "Churrasco",
     price: 950.00,
     description: "Cortes nobres, acompanhamentos deliciosos e um toque de sofisticação. Ideal para quem deseja transformar o churrasco tradicional em um evento de alta gastronomia.",
-    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1736888894/pasta-beef-restaurant-gourmet-flavor-italian-specialties-ambiance-chefs-dishes-quality_1246444-74550_f6q7l5.jpg",
+    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1745334727/pasta-beef-restaurant-gourmet-flavor-italian-specialties-ambiance-chefs-dishes-quality_1246444-74550_f6q7l5_lr5qpf.jpg",
     user: users[2]
   },
   {
@@ -46,7 +46,7 @@ services_data = [
     category: "Sobremesas Finas",
     price: 420.00,
     description: "Uma seleção exclusiva de sobremesas artesanais, desde mousses e tortas até doces finos. Personalize o cardápio para combinar com o tema do seu evento.",
-    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1736888970/d3bd6c16a48f5918745252708c3634d1_lq0kby.jpg",
+    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1745334778/d3bd6c16a48f5918745252708c3634d1_lq0kby_nyefox.jpg",
     user: users[0]
   },
   {
@@ -54,7 +54,7 @@ services_data = [
     category: "Cozinha Internacional",
     price: 750.00,
     description: "Explore sabores autênticos de diferentes culturas em um menu que passa por pratos mexicanos, tailandeses, franceses e mais. Uma viagem gastronômica sem sair de casa.",
-    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1736889011/gastronomia-enjoy-blog-1024x682_p8zoah.jpg",
+    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1745334802/gastronomia-enjoy-blog-1024x682_p8zoah_coequl.jpg",
     user: users[1]
   },
   {
@@ -62,7 +62,7 @@ services_data = [
     category: "Culinária Japonesa",
     price: 890.00,
     description: "Sushis frescos preparados ao vivo por um chef especializado. Inclui uma variedade de opções, como nigiris, sashimis e rolls personalizados.",
-    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1736889057/rodizios-min-1_nd9kas.jpg",
+    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1745334825/rodizios-min-1_nd9kas_enyhkp.jpg",
     user: users[1]
   },
   {
@@ -70,7 +70,7 @@ services_data = [
     category: "Gastronomia Espanhola",
     price: 880.00,
     description: "Desfrute de uma paella tradicional valenciana, preparada com ingredientes frescos como frutos do mar, açafrão e arroz importado. Perfeita para jantares em família ou eventos.",
-    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1736889127/3.-Paella-Valenciana_wakksd.jpg",
+    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1745334846/3.-Paella-Valenciana_wakksd_gdrygv.jpg",
     user: users[2]
   },
   {
@@ -78,7 +78,7 @@ services_data = [
     category: "Gastronomia Brasileira",
     price: 600.00,
     description: "Uma feijoada rica e completa, com carnes selecionadas, acompanhamentos como couve, farofa e laranja, e sobremesas típicas brasileiras.",
-    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1736889224/feijoada-wooden-table-traditional-brazilian-food_123827-26007_nswexr.jpg",
+    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1745334856/feijoada-wooden-table-traditional-brazilian-food_123827-26007_nswexr_r9du4j.jpg",
     user: users[2]
   },
   {
@@ -86,7 +86,7 @@ services_data = [
     category: "Café da Manhã",
     price: 400.00,
     description: "Um café da manhã completo com pães artesanais, bolos caseiros, geleias frescas, sucos naturais e um toque especial de hospitalidade.",
-    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1736893855/yukiko-kanada-Ou4CQo6jzvU-unsplash-1024x692_owwyaf.jpg",
+    photo_url: "https://res.cloudinary.com/ddbg7qknd/image/upload/v1745334865/yukiko-kanada-Ou4CQo6jzvU-unsplash-1024x692_owwyaf_lgzhcu.jpg",
     user: users[1]
   }
 ]
